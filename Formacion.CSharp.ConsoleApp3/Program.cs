3ffee8241b7b73249112c7a6c4c7b41cdf816668
Francisco de Borja Cabeza Rozas <borjacabezarozas@gmail.com>
2024-05-24 13:54:06 +0200

    Commit 4, día 24-05-2024

--- Formacion.CSharp.ConsoleApp3/Program.cs
+++ Formacion.CSharp.ConsoleApp3/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Formacion.CSharp.ConsoleApp3
+﻿using System.Collections;
+
+namespace Formacion.CSharp.ConsoleApp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Arrays();
+            Lists();
         }
 
         static void Arrays()
@@ -66,7 +68,7 @@
 
             Console.WriteLine($"Valor [1][2]: {jaggedArray[1][2]}");
 
-            // Eliminar elementos o posiciones de un Array
+            // Asignar valores por defecto
             int[] numeros = { 15, -3, 577, 82, 19, 33, 78, 1000, 63 };
             Console.WriteLine($"Número de Elementos: {numeros.Length}");
             foreach (int i in numeros) Console.Write($"{i} - ");
@@ -89,6 +91,72 @@
             frutas[10] = "manzana";
             foreach (string i in frutas) Console.Write($"{i} - ");
 
+        }
+
+        static void ArrayLists()
+        {
+            // Instanciar
+            ArrayList array = new ArrayList();
+
+            // Eliminar todos los elementos del Array
+            array.Clear();
+
+            // Añadir nuevo elementos
+            array.Add(1);
+            array.Add("borja");
+            array.Add(new Alumno() { Nombre = "Borja", Edad = 49 });
+            array.Add(new { Nombre = "Borja", Apellidos = "Cabeza" });
+
+            array.AddRange(new string[] { "manzana", "pera", "melocoton" });
+
+            ArrayList array2 = new ArrayList();
+            array2.Add(30);
+            array2.Add(15);
+            array.AddRange(array2);
+
+            // Añadir un elemento en una posición del array
+            array.Insert(2, "blanco");
+            array.InsertRange(2, new string[] { "rojo", "verde", "azul" });
+
+
+            // Eliminar elementos del ArrayList
+            array.Remove("blanco");
+            array.RemoveAt(4);
+            array.RemoveRange(2, 2);
+
+            // Saber si un elemento esta contenido en el ArrayList
+            Console.WriteLine($"Contiene el item pera: {array.Contains("pera")}");
+
+            // Ordenar elementos
+            array.Sort();
+
+            // Invertir los elementos
+            array.Reverse();
+
+            // Convertir el ArrayList en un array -> object[] nuevoArray
+            object[] nuevoArray = array.ToArray();
+
+            array.
+
+            // Recorremos el ArrayList
+            foreach (var item in array)
+                Console.WriteLine($"Item: {item.GetType().ToString()}");
+
+            // Número de elementos del ArrayList
+            Console.WriteLine($"Número de elementos: {array.Count}");
+        }
+
+        static void Lists()
+        { 
+            // Instanciar List
+            List<string> lista = new List<string>();
+
+            List<string> lista2 = new();
+            var lista3 = new List<string>();
+
+            List<int> lista4 = new List<int>();
+            List<Alumno> lista5 = new List<Alumno>();
+     
         }
     }
 
