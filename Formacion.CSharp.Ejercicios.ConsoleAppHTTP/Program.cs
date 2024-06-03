1812aa4cc77ad259a934ecb8532f462de7a11074
Francisco de Borja Cabeza Rozas <borjacabezarozas@gmail.com>
2024-06-03 12:53:10 +0200

    Commit 5, día 03-06-2024

--- Formacion.CSharp.Ejercicios.ConsoleAppHTTP/Program.cs
+++ Formacion.CSharp.Ejercicios.ConsoleAppHTTP/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Newtonsoft.Json;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 
 namespace Formacion.CSharp.Ejercicios.ConsoleAppHTTP
 {
@@ -10,7 +12,7 @@ namespace Formacion.CSharp.Ejercicios.ConsoleAppHTTP
         {
             http = new HttpClient();
 
-            ConsultarCodigoPostal2();
+            ConsultarCodigoPostal3();
         }
 
         // Método GET
@@ -52,7 +54,7 @@ namespace Formacion.CSharp.Ejercicios.ConsoleAppHTTP
                 Console.WriteLine($"Código Postal: {data.PostCode}");
 
                 foreach (var place in data.Places)
-                    Console.WriteLine($" -> {place.PlaceName} ({place.State}) - {place.StateCode}");
+                    Console.WriteLine($" -> {place.PlaceName} ({place.State})");
             }
             else Console.WriteLine($"Error: {response.StatusCode}");
 
@@ -68,25 +70,16 @@ namespace Formacion.CSharp.Ejercicios.ConsoleAppHTTP
 
             try
             {
-
-            }
-            catch (Exception)
-            {
-                
-            }
-
-            var response = http.GetAsync($"https://api.zippopotam.us/es/{code}").Result;
-            if (response.IsSuccessStatusCode)
-            {
-                var data = JsonConvert.DeserializeObject<PostalCodeInfo>(response.Content.ReadAsStringAsync().Result);
-
+                var data = http.GetFromJsonAsync<PostalCodeInfo>($"https://api.zippopotam.us/es/{code}").Result;
                 Console.WriteLine($"Código Postal: {data.PostCode}");
 
                 foreach (var place in data.Places)
-                    Console.WriteLine($" -> {place.PlaceName} ({place.State}) - {place.StateCode}");
+                    Console.WriteLine($" -> {place.PlaceName} ({place.State})");
             }
-            else Console.WriteLine($"Error: {response.StatusCode}");
-
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
         }
     }
 
@@ -94,10 +87,12 @@ namespace Formacion.CSharp.Ejercicios.ConsoleAppHTTP
     public class PostalCodeInfo
     {
         [JsonProperty("post code")]
+        [JsonPropertyName("post code")]
         public string PostCode {get; set;}
         public string Country {get; set;}
 
         [JsonProperty("country abbreviation")]
+        [JsonPropertyName("country abbreviation")]
         public string CountryAbbreviation { get; set;}
         public List<PostalCodePlace> Places {get; set;}
     }
@@ -105,12 +100,14 @@ namespace Formacion.CSharp.Ejercicios.ConsoleAppHTTP
     public class PostalCodePlace
     {
         [JsonProperty("place name")]
+        [JsonPropertyName("place name")]
         public string PlaceName { get; set; }
         public string Longitude { get; set; }
         public string Latitude { get; set; }
         public string State { get; set; }
 
         [JsonProperty("state abbreviation")]
+        [JsonPropertyName("state abbreviation")]
         public string StateCode { get; set; }
     }
 
