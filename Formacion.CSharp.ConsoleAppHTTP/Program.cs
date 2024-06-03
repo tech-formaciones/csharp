6628f03771af4c1f5ff72ee694e5a7dc00842f79
Francisco de Borja Cabeza Rozas <borjacabezarozas@gmail.com>
2024-06-03 10:20:33 +0200

    Commit, día 03-06-2024

--- Formacion.CSharp.ConsoleAppHTTP/Program.cs
+++ Formacion.CSharp.ConsoleAppHTTP/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Formacion.CSharp.ConsoleAppHTTP
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Formacion.CSharp.ConsoleAppHTTP
 {
     internal class Program
     {
@@ -18,15 +21,77 @@
             http.DefaultRequestHeaders.Clear();
             http.DefaultRequestHeaders.Add("User-Agent", "HttpCliente .NET Core Demo");
             http.DefaultRequestHeaders.Add("X-Data-1", "Demo");
-            http.DefaultRequestHeaders.Add("X-Data-2", "1234567890");            
+            http.DefaultRequestHeaders.Add("X-Data-2", "1234567890");
+
+
+            // Llamada a GET
+            Get();
         }
 
         static void Get()
         {
             // get
 
-            HttpResponseMessage response = http.GetAsync("get").Result;
-        
+            HttpResponseMessage response = http.GetAsync("get?param1=hola").Result;
+
+            // Opción 1, preguntamos por un código de estado concreto.
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                // Lectura del cuerpo del mensaje de respuesta como STRING
+                // El texto obtenido normalmente estara en JSON
+
+                string responseBodyText = response.Content.ReadAsStringAsync().Result;
+
+                Console.WriteLine("Body:");
+                Console.WriteLine("=============================================");
+                Console.WriteLine($"{responseBodyText}");
+
+
+                // Convertir el JSON en Objeto, indicado el tipo
+                // Utilizamos DYNAMIC cuando no tenemos una clase que represente el objeto retornado
+
+                var obj = JsonConvert.DeserializeObject<dynamic>(responseBodyText);
+
+                Console.WriteLine("");
+                Console.WriteLine("Datos del Body:");
+                Console.WriteLine("=============================================");
+                Console.WriteLine($"URL: {obj["url"]}");
+                Console.WriteLine($"Param 1: {obj["args"]["param1"]}");
+                Console.WriteLine($"Data 1: {obj["headers"]["x-data-1"]}");
+
+                Console.WriteLine($"URL: {obj.url}");
+                Console.WriteLine($"Param 1: {obj.args.param1}");
+
+                Console.WriteLine("");
+                Console.WriteLine("Headers:");
+                Console.WriteLine("=============================================");
+                // También tenemos acceso a las cabeceras del mensaje de respuesta
+                foreach (var header in response.Headers)
+                {
+                    Console.WriteLine($"{header.Key}: {header.Value.FirstOrDefault()}");
+                }
+
+                Console.WriteLine($"Date: {response.Headers.GetValues("Date").FirstOrDefault()}");
+
+                IEnumerable<string> valor;
+                response.Headers.TryGetValues("Content-Type", out valor);
+
+                Console.WriteLine($"Content-Type: {(valor == null ? "" : valor.FirstOrDefault())}");
+
+            }
+            else Console.WriteLine($"Error: {response.StatusCode}");
+
+
+            // Opción 2, preguntamos si finalizada con cualquier códgo 2XX
+
+            if (response.IsSuccessStatusCode)
+            {
+                // El mismo código que la opción 1
+            }
+            else Console.WriteLine($"Error: {response.StatusCode}");
         }
     }
+
+
 }
