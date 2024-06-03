0898e9e93c50e53933a6555a4fbb6274fec25e27
Francisco de Borja Cabeza Rozas <borjacabezarozas@gmail.com>
2024-06-03 10:56:30 +0200

    Commit 2, día 03-06-2024

--- Formacion.CSharp.ConsoleAppHTTP/Program.cs
+++ Formacion.CSharp.ConsoleAppHTTP/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace Formacion.CSharp.ConsoleAppHTTP
 {
@@ -23,9 +25,11 @@ namespace Formacion.CSharp.ConsoleAppHTTP
             http.DefaultRequestHeaders.Add("X-Data-1", "Demo");
             http.DefaultRequestHeaders.Add("X-Data-2", "1234567890");
 
+            // http.DefaultRequestHeaders.Add("Accept", "application/json");
+            // http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
             // Llamada a GET
-            Get();
+            Get2();
         }
 
         static void Get()
@@ -91,6 +95,87 @@ namespace Formacion.CSharp.ConsoleAppHTTP
             }
             else Console.WriteLine($"Error: {response.StatusCode}");
         }
+
+        static void Get2()
+        {
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "get?param1=hola");
+
+            //HttpRequestMessage request = new HttpRequestMessage();
+            //request.Method = HttpMethod.Get;
+            //request.RequestUri = new Uri("https://postman-echo.com/get?param1=hola");
+
+            // Opcionalmente definimos cabeceras del mensaje
+            // Las cabeceras se anexan a las cabeceras por defecto del objeto HttpClient
+
+            request.Headers.Add("X-Data-3", "ABCD");
+            request.Headers.Add("Accept", "application/json");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // Opcionalmente definimos el cuerpo del mensaje
+
+            var obj = new { Nombre = "Borja", Apellido1 = "Cabeza", Apellido2 = "Rozas" };
+
+            // Opción 1
+            // request.Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+
+            // Opción 2
+            string objJSON = JsonConvert.SerializeObject(obj);
+            StringContent contenido = new StringContent(objJSON, Encoding.UTF8, "application/json");
+            request.Content = contenido;
+
+
+            // Enviamos la petición y obtenemos la respuesta
+
+            HttpResponseMessage response = http.SendAsync(request).Result;
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                // Lectura del cuerpo del mensaje de respuesta como STRING
+                // El texto obtenido normalmente estara en JSON
+
+                string responseBodyText = response.Content.ReadAsStringAsync().Result;
+
+                Console.WriteLine("Body:");
+                Console.WriteLine("=============================================");
+                Console.WriteLine($"{responseBodyText}");
+
+
+                // Convertir el JSON en Objeto, indicado el tipo
+                // Utilizamos DYNAMIC cuando no tenemos una clase que represente el objeto retornado
+
+                var obj2 = JsonConvert.DeserializeObject<dynamic>(responseBodyText);
+
+                Console.WriteLine("");
+                Console.WriteLine("Datos del Body:");
+                Console.WriteLine("=============================================");
+                Console.WriteLine($"URL: {obj2["url"]}");
+                Console.WriteLine($"Param 1: {obj2["args"]["param1"]}");
+                Console.WriteLine($"Data 1: {obj2["headers"]["x-data-1"]}");
+
+                Console.WriteLine($"URL: {obj2.url}");
+                Console.WriteLine($"Param 1: {obj2.args.param1}");
+
+                Console.WriteLine("");
+                Console.WriteLine("Headers:");
+                Console.WriteLine("=============================================");
+                // También tenemos acceso a las cabeceras del mensaje de respuesta
+                foreach (var header in response.Headers)
+                {
+                    Console.WriteLine($"{header.Key}: {header.Value.FirstOrDefault()}");
+                }
+
+                Console.WriteLine($"Date: {response.Headers.GetValues("Date").FirstOrDefault()}");
+
+                IEnumerable<string> valor;
+                response.Headers.TryGetValues("Content-Type", out valor);
+
+                Console.WriteLine($"Content-Type: {(valor == null ? "" : valor.FirstOrDefault())}");
+
+            }
+            else Console.WriteLine($"Error: {response.StatusCode}");
+        }
+
+
     }
 
 
